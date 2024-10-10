1ed738b2265a147eda3db063a570dd05b7fc2244
vitayusko <vitayusko@users.noreply.github.com>
2024-10-10 13:11:28 +0200

    comment

--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -1,3 +1,5 @@
+// src/utils/calculatePaginationData.js
+
 export const calculatePaginationData = (count, perPage, page) => {
   const totalPages = Math.ceil(count / perPage);
   const hasNextPage = page < totalPages;
