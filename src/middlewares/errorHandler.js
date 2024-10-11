8e6983cb25cea181ff70b078c33bda269fc60249
vitayusko <vitayusko@users.noreply.github.com>
2024-10-11 08:57:14 +0200

    request update

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -2,13 +2,12 @@
 
 export const errorHandler = (err, req, res, next) => {
   const status = err.status || 500;
-  const message = err.message
-    ? err.message
-    : 'No specific error message provided';
+  const message = err.message || 'Internal Server Error';
+  const details = err.details || null;
 
   res.status(status).json({
     status: status,
     message: message,
-    data: message,
+    details: details,
   });
 };
--- src/middlewares/validateBody.js
+++ src/middlewares/validateBody.js
@@ -7,9 +7,12 @@ export const validateBody = (schema) => async (req, res, next) => {
     await schema.validateAsync(req.body, { abortEarly: false });
     next();
   } catch (err) {
-    const error = createHttpError(400, 'Bad Request', {
-      errors: err.details,
-    });
+    const details = err.details.map((detail) => ({
+      message: detail.message,
+      path: detail.path,
+    }));
+
+    const error = createHttpError(400, 'Validation error', { details });
     next(error);
   }
 };
